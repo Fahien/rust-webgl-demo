c54747f8f8c65e21ac2a5984f0771fb82e142ea9
Fahien <Fahien@users.noreply.github.com>
2020-12-15 12:32:53 +0000

    deploy: 43d3e38fecc3448706e2e4ddf72217d8760f6de7

--- bootstrap.js
+++ bootstrap.js
@@ -462,7 +462,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/webgl_bg.wasm":"793bbbb46cc637ffd700"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/webgl_bg.wasm":"2b2763d8afbb9cd9bfb2"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
