d7de2b913bc67db2fa78e4f99b55a49dbfe81c05
Antonio Caggiano <info@antoniocaggiano.eu>
2020-12-15 13:55:46 +0100

    Fix GL context creation

--- www/index.js
+++ www/index.js
@@ -1,14 +1,31 @@
 import * as webgl from "webgl";
 
+var slideshow = remark.create();
+
+slideshow.on("afterShowSlide", function(slide) {
+    if (slide.getSlideIndex() == 5) {
+        start_demo();
+    }
+    if (slide.getSlideIndex() == 0) {
+        start_teaser();
+    }
+})
+
 var ctx = null;
-var teaser_ctx = webgl.Context.new_teaser();
+var teaser_ctx = null;
+
 
 const tick_teaser = () => {
     teaser_ctx.draw_teaser();
     requestAnimationFrame(tick_teaser);
 }
 
-requestAnimationFrame(tick_teaser);
+function start_teaser() {
+    if (teaser_ctx == null) {
+        teaser_ctx = webgl.Context.new_teaser();
+    }
+    requestAnimationFrame(tick_teaser);
+}
 
 const tick = () => {
     ctx.draw();
@@ -25,3 +42,10 @@ function start_demo() {
     }
     requestAnimationFrame(tick);
 }
+
+if (slideshow.getCurrentSlideIndex() == 0) {
+    start_teaser();
+} else if (slideshow.getCurrentSlideIndex() == 5) {
+    start_demo();
+}
+
